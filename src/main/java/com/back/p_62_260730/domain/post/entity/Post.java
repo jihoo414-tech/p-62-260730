e1fcc336361e9aedf65590fc4c96b53fc425b792
jihoo414-tech <jihoo414@gmail.com>
2026-07-31 10:27:26 +0900

    ㅔPost entity

--- src/main/java/com/back/p_62_260730/domain/post/entity/Post.java
+++ src/main/java/com/back/p_62_260730/domain/post/entity/Post.java
@@ -1,10 +1,7 @@
 package com.back.p_62_260730.domain.post.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,11 +13,13 @@ import lombok.Setter;
 public class Post {
 
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     @Column(columnDefinition = "TEXT" )
     private String content;
+
+
 
     public Post(String title, String content){
         this.title = title;
