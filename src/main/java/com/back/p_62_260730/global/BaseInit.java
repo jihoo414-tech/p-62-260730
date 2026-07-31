d28fb62860ae8a7031efeafa5b15c44168f40644
jihoo414-tech <jihoo414@gmail.com>
2026-07-31 11:21:03 +0900

    service annotation

--- src/main/java/com/back/p_62_260730/global/BaseInit.java
+++ src/main/java/com/back/p_62_260730/global/BaseInit.java
@@ -1,17 +1,19 @@
 package com.back.p_62_260730.global;
 
 import com.back.p_62_260730.domain.post.service.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class BaseInit {
 
+    private final PostService postService;
 
-    @Autowired
-    PostService postService;
+
+
     @Bean
     ApplicationRunner initDataRunner(){
         return  args -> {
