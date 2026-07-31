a6b0cb3b81aa8ddd1cd0d33135880d9b02c0cbc9
jihoo414-tech <jihoo414@gmail.com>
2026-07-31 09:49:59 +0900

    findById

--- src/main/java/com/back/p_62_260730/global/BaseInit.java
+++ src/main/java/com/back/p_62_260730/global/BaseInit.java
@@ -7,6 +7,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Optional;
+
 @Configuration
 public class BaseInit {
 
@@ -17,8 +19,10 @@ public class BaseInit {
     @Bean
     ApplicationRunner initDataRunner(){
         return  args -> {
-            System.out.println("post의 row 개수를 셉니다.");
-            postRepository.count();
+            System.out.println("초기화 작업을 수행합니다.");
+            if(postRepository.count() > 0){
+                return;
+            }
 
             //post 하나 저장
             Post post = new Post();
@@ -27,6 +31,12 @@ public class BaseInit {
             postRepository.save(post);
 
             //post 조회
+           Optional<Post> opPost =  postRepository.findById(1);
+
+           if(opPost.isPresent()){
+               System.out.println(opPost.get().getTitle());
+               System.out.println(opPost.get().getContent());
+           }
         };
     }
 }
