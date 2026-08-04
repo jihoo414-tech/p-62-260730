90c9c177f8f585fd9e844415b1486144c5462574
jihoo414-tech <jihoo414@gmail.com>
2026-08-04 10:31:21 +0900

    testDB

--- src/main/java/com/back/p_62_260730/global/BaseEntity.java
+++ src/main/java/com/back/p_62_260730/global/BaseEntity.java
@@ -1,16 +1,19 @@
 package com.back.p_62_260730.global;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-
+@Getter
+@Setter
 @MappedSuperclass // 상속용으로 존재하는 클래스. -> 테이블 생성 안됨
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
+public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
